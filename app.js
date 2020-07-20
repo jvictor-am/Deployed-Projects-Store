f15ca438f4169a78a299387817a79196feb2ce15
Joao Mesquita <joaovictorengp@gmail.com>
2020-07-20 04:36:18 -0300

    edit project page and logic implemented

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const exphbs = require('express-handlebars');
+const methodOverride = require('method-override');
 const passport = require('passport');
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
@@ -22,16 +23,33 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+app.use(
+  methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+      // look in urlencoded POST bodies and delete it
+      var method = req.body._method;
+      delete req.body._method;
+      return method;
+    }
+  })
+);
+
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-const { formatDate, truncate, stripTags, editIcon } = require('./helpers/hbs');
+const {
+  formatDate,
+  truncate,
+  stripTags,
+  editIcon,
+  select,
+} = require('./helpers/hbs');
 
 app.engine(
   '.hbs',
   exphbs({
-    helpers: { formatDate, truncate, stripTags, editIcon },
+    helpers: { formatDate, truncate, stripTags, editIcon, select },
     efaultLayout: 'main',
     extname: '.hbs',
   })
--- routes/projects.js
+++ routes/projects.js
@@ -31,7 +31,39 @@ router.get('/', ensureAuth, async (req, res) => {
     console.error(err);
     res.render('error/500');
   }
-  // res.render('projects/add');
+});
+
+router.get('/edit/:id', ensureAuth, async (req, res) => {
+  const project = await Project.findOne({ _id: req.params.id }).lean();
+
+  if (!project) {
+    return res.render('error/404');
+  }
+
+  if (project.user != req.user.id) {
+    res.redirect('/projects');
+  } else {
+    res.render('projects/edit', { project });
+  }
+});
+
+router.put('/:id', ensureAuth, async (req, res) => {
+  let project = await Project.findById(req.params.id).lean();
+
+  if (!project) {
+    return res.render('error/404');
+  }
+
+  if (project.user != req.user.id) {
+    res.redirect('/projects');
+  } else {
+    project = await Project.findOneAndUpdate({ _id: req.params.id }, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.redirect('/dashboard');
+  }
 });
 
 module.exports = router;
